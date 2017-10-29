a4d68f4aa684a88e74802054858ee188f18122ff
MachineMitch21 <mitchschutt@outlook.com>
2017-10-29 10:40:09 -0400

    Added some comments to the ArgumentParser.parseArgs method

--- src/ArgumentParser.cpp
+++ src/ArgumentParser.cpp
@@ -30,10 +30,14 @@ namespace lc
     {
         std::string command;
 
+        // There should at least be a directory specified as the first arguments
+        // If not, we return with a PARSE_INVALID_DIRECTORY error code
         if (argc > 1)
         {
             _directory = argv[1];
 
+            // If there are more than two arguments ([program-name] [directory])
+            // Then we start parsing the extra arguments, looking for commands
             if (argc > 2)
             {
                 for (int i = 2; i < argc; i++)
@@ -44,12 +48,18 @@ namespace lc
                     {
                         command = arg;
 
+                        // If we have found a command, then we want to start looping
+                        // over the following arguments until we hit another command
+                        // or we run out of arguments
                         for (int j = i + 1; j < argc; j++)
                         {
                             std::string arg(argv[j]);
 
                             if (!isCommand(arg))
                             {
+                                // If this argument isn't a command we need to push
+                                // it onto the argument list of the current command
+                                // that we previously found
                                 _cmdArgs[_commands[command]].push_back(arg);
                                 i++;
                             }
